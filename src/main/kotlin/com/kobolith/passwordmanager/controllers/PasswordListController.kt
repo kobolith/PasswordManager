4e79c75a6903627e297ec2ef10057245112cac8c
Luke Idenburg <luke@kobolith.com>
2024-12-02 14:21:50 +0100

    Changes to the main screen, slight changes

--- src/main/kotlin/com/kobolith/passwordmanager/controllers/PasswordListController.kt
+++ src/main/kotlin/com/kobolith/passwordmanager/controllers/PasswordListController.kt
@@ -1,13 +1,16 @@
 package com.kobolith.passwordmanager.controllers
 
+import com.kobolith.passwordmanager.models.PasswordList
 import com.kobolith.passwordmanager.views.PasswordListView
 
 class PasswordListController: Controller() {
 
+    private var passwordList: PasswordList = PasswordList()
     private val view: PasswordListView = PasswordListView()
 
     init {
-
+        view.getListView().items = passwordList.getCredentials()
+        view.getLabel().text = "Number of Passwords: ${view.getListView().items.size}"
     }
 
     override fun getView(): PasswordListView {
--- src/main/kotlin/com/kobolith/passwordmanager/views/PasswordListView.kt
+++ src/main/kotlin/com/kobolith/passwordmanager/views/PasswordListView.kt
@@ -1,13 +1,17 @@
 package com.kobolith.passwordmanager.views
 
+import com.kobolith.passwordmanager.models.Credential
 import javafx.scene.Parent
 import javafx.scene.control.Label
+import javafx.scene.control.ListView
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
 
 class PasswordListView: View() {
 
     private lateinit var root: Parent
+    private lateinit var listView: ListView<Credential>
+    private lateinit var label: Label
 
     init {
         initializeRoot()
@@ -16,8 +20,13 @@ class PasswordListView: View() {
     private fun initializeRoot() {
         val outerBox = VBox()
         val innerBox = HBox()
+        listView = ListView<Credential>()
+        listView.setCellFactory { CredentialsListCell() }
 
-        innerBox.children.add(Label("Password List"))
+        label = Label("Number of Passwords: ${listView.items.size}")
+
+        innerBox.children.add(listView)
+        innerBox.children.add(label)
         outerBox.children.add(innerBox)
 
         root = outerBox
@@ -26,4 +35,13 @@ class PasswordListView: View() {
     override fun getRoot(): Parent {
         return root
     }
+
+    fun getListView(): ListView<Credential> {
+        return listView
+    }
+
+    fun getLabel(): Label {
+        return label
+    }
+
 }
