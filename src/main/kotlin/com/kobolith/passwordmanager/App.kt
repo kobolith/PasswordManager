6d2716f049533cb16696e3083590479448ce4c5c
Luke Idenburg <luke@kobolith.com>
2024-12-02 12:16:51 +0100

    Added Config object for application constants. Created switchController method for easy stage view swapping, added application icon, and error handling for missing app icon.

--- src/main/kotlin/com/kobolith/passwordmanager/App.kt
+++ src/main/kotlin/com/kobolith/passwordmanager/App.kt
@@ -1,22 +1,56 @@
 package com.kobolith.passwordmanager
 
+import com.kobolith.passwordmanager.controllers.Controller
+import com.kobolith.passwordmanager.controllers.PasswordListController
+
 import javafx.application.Application
-import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.image.Image
+import javafx.scene.layout.Pane
 import javafx.stage.Stage
 
 
-const val APP_TITLE = "Password Manager"
-const val APP_VERSION = "0.1.0"
-
-
-
 class App : Application() {
+    object Config {
+        const val APP_TITLE = "Password Manager"
+        const val APP_VERSION = "0.1.0"
+        const val APP_ICON_PATH = "/app_icon.png"
+
+        const val APP_INITIAL_WIDTH = 800.0
+        const val APP_INITIAL_HEIGHT = 600.0
+    }
+
+    private lateinit var stage: Stage
+
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(App::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
-        stage.title = "$APP_TITLE v$APP_VERSION"
-        stage.scene = scene
+        this.stage = stage
+        this.stage.scene = Scene(Pane())
+        this.stage.title = "${Config.APP_TITLE} v${Config.APP_VERSION}"
+
+        this.stage.width = Config.APP_INITIAL_WIDTH
+        this.stage.height = Config.APP_INITIAL_HEIGHT
+
+        try {
+            val image = Image(
+                javaClass.getResource(Config.APP_ICON_PATH)?.toExternalForm()
+                    ?: throw IllegalArgumentException("Resource not found: ${Config.APP_ICON_PATH}")
+            )
+            this.stage.icons.add(image)
+        } catch (e: IllegalArgumentException) {
+            System.err.println(e.message)
+        }
+
+        switchController(PasswordListController())
+    }
+
+
+    /**
+     * Loads any controller's view and switches to it
+     *
+     * @param controller The controller to switch to
+     */
+    fun switchController(controller: Controller) {
+        stage.scene.root = controller.getView().getRoot()
         stage.show()
     }
 }
